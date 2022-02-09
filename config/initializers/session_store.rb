3d1686259eb4c202ed56888c122d1226d3759894
ArtemS <smoilovskyi151@gmail.com>
2022-02-09 18:37:22 +0100

    Session change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_auth_todo", domain: "https://tasks-react-ts.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_auth_todo", domain: "https://tasks-ruby.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_auth_todo"
 end
